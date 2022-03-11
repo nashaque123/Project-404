6d2c9344813d2adb84e8c5d9b8ef1e9f45f00e58
HAQUE <S6061420@live.tees.ac.uk>
2022-03-11 14:21:31 +0000

    renamed from manager to controller

--- Assets/GameScripts/AgentController.cs
+++ Assets/GameScripts/AgentController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour
+public class AgentController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animal thisAnimal;
@@ -17,13 +17,13 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveInput = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
         rb.MovePosition(rb.position + (thisAnimal.StepSize * Time.deltaTime * moveInput.normalized));
         thisAnimal.Stamina -= thisAnimal.StaminaCost;
-
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("joystick button 0"))
-        {
-            //attack
-        }
     }
 }
--- Assets/GameScripts/PlayerController.cs
+++ Assets/GameScripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AgentManager : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animal thisAnimal;
@@ -17,13 +17,13 @@ public class AgentManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-
-    void Move()
-    {
-        Vector3 moveInput = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         rb.MovePosition(rb.position + (thisAnimal.StepSize * Time.deltaTime * moveInput.normalized));
         thisAnimal.Stamina -= thisAnimal.StaminaCost;
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("joystick button 0"))
+        {
+            //attack
+        }
     }
 }
