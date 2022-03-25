29cc432857d163a690c84dc3d69c8863cf6bd8f2
LittleLozza12 <64976360+LittleLozza12@users.noreply.github.com>
2022-03-25 14:54:59 +0000

    Alignment, Seperation & Cohesion

--- Assets/Flocking/flocking.cs
+++ Assets/Flocking/flocking.cs
@@ -8,6 +8,13 @@ public class flocking : MonoBehaviour
     public SphereCollider neighbourCollider;
     public int agentNumber = 5;
     public List<GameObject> neighbours;
+    public Vector3 velocity;
+
+    public float alignmentFactor=0.1f;
+    public float cohesionFactor = 0.1f;
+    public float seperationFactor = 0.1f;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,17 +22,56 @@ public class flocking : MonoBehaviour
         neighbourCollider = gameObject.GetComponent<SphereCollider>();
         neighbourCollider.radius = radius;
         neighbours = new List<GameObject>();
-        //Debug.Log(neighbours);
+        velocity = new Vector3(Random.Range(0, 20f), Random.Range(0, 20f), Random.Range(0, 20f));
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 alignemt = new Vector3(0, 0, 0);
         //Alignment
 
+
+
+
+        //for each agent
+
+        foreach (GameObject neighbour in neighbours)
+        {
+            alignemt += neighbour.GetComponent<flocking>().velocity; 
+        }
+        //Add velocity
+        //end for
+        //divide by number of agents
+
+        alignemt = alignemt/neighbours.Count;
+
+
         //Cohesion
+        Vector3 cohesion = new Vector3(0, 0, 0);
+
+        foreach (GameObject neighbour in neighbours)
+        {
+            cohesion += neighbour.transform.position;
+        }
+
+        cohesion = cohesion / neighbours.Count;
+
 
         //Seperation
+        Vector3 seperation = new Vector3(0, 0, 0);
+
+        foreach (GameObject neighbour in neighbours)
+        {
+            seperation += gameObject.transform.position - neighbour.transform.position;
+        }
+
+        seperation = seperation / neighbours.Count;
+
+        velocity = (alignmentFactor * alignemt + cohesionFactor * cohesion + seperationFactor * seperation);
+
+        //Movement
+        transform.Translate(velocity * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider collision)
