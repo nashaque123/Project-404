4ecee5d261f431b53e88b62e4b81ecb6b9891616
Sara Rushmer <57770253+SaraR-GameProgramming@users.noreply.github.com>
2022-04-01 04:00:42 +0100

    Create grid
    creating grid for pathfinding

---
+++ Assets/Pathfinding/GridBehaviour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehaviour : MonoBehaviour
+{
+
+    public int gridRows = 20;
+    public int gridColumns = 20;
+    public int scale = 1;
+    public GameObject gridPrefab;
+    public Vector3 bottomLeftLocation = new Vector3(0,0,0);
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (gridPrefab)
+            GridGeneration();
+        else print("no prefab. assign prefab!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GridGeneration()
+    {
+        for (int i = 0; i < gridColumns; i++)
+        {
+            for (int j = 0; j < gridRows; j++)
+            {
+                GameObject obj = Instantiate(gridPrefab, new Vector3(bottomLeftLocation.x + scale * i, bottomLeftLocation.y, bottomLeftLocation.z + scale * j),Quaternion.identity);
+                obj.transform.SetParent(gameObject.transform);
+                obj.GetComponent<GridInfo>().x = i;
+                obj.GetComponent<GridInfo>().x = j;
+            }
+        }
+    }
+}
